a61d2d8b9d4a783bd6d3d40ee7b07ab4c1217e3c
OrshiKozek <okozek@dons.usfca.edu>
2018-10-22 17:35:12 -0700

    ArrayList.java added

---
+++ ArrayList.java
@@ -0,0 +1,35 @@
+import java.lang.reflect.Array;
+import java.util.*;
+public class ArrayList implements List {
+
+
+    public static List list = new ArrayList();
+    public static int size = 0;
+
+
+    @Override
+    public void add(Object obj) {
+
+    }
+
+    @Override
+    public void add(int pos, Object obj) {
+
+    }
+
+    @Override
+    public Object remove(int pos) {
+        return null;
+    }
+
+    @Override
+    public Object get(int pos) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+}
