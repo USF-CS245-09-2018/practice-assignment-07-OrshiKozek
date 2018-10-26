9b2c2678e54b498251da18358da3d33fee75f4eb
OrshiKozek <okozek@dons.usfca.edu>
2018-10-26 15:22:33 -0700

    ArrayList.java updated, completed

--- ArrayList.java
+++ ArrayList.java
@@ -1,35 +1,78 @@
-import java.lang.reflect.Array;
-import java.util.*;
+import javax.sound.midi.Soundbank;
+import java.lang.Exception;
 public class ArrayList implements List {
 
 
-    public static List list = new ArrayList();
-    public static int size = 0;
+    public static Object[] list = new Object[10];
+    public static int size;
+    public static int end = -1;
 
 
     @Override
     public void add(Object obj) {
-
+        if(list[list.length-1]!=null)
+            growList();
+        end++;
+        list[end] = obj;
+        size++;
     }
 
     @Override
     public void add(int pos, Object obj) {
-
+        if(list[list.length-1]!=null)
+            growList();
+        int i = end;
+        if(list[pos] != null) {
+            while (list[i] != null) {
+                list[i + 1] = list[i];
+                i--;
+            }
+        }
+        list[pos] = obj;
+        end++;
+        size++;
     }
 
     @Override
     public Object remove(int pos) {
-        return null;
+        Object temp = list[pos];
+        int i = pos;
+        while (pos<=end){
+            try {
+                list[pos] = list[pos+1];
+                pos++;
+            }
+            catch (Exception e){
+                System.out.println("Last index out of bounds- add null manually");
+                list[pos] = null;
+            }
+        }
+        end--;
+        size--;
+        return temp;
     }
 
     @Override
     public Object get(int pos) {
-        return null;
+        if (pos > size-1){
+            System.out.println("Invalid index");
+            return null;
+        }
+        return list[pos];
     }
 
     @Override
     public int size() {
         return size;
+    }
+
+    public void growList(){
+        Object[] newArray = new Object[list.length*2];
+        int index = 0;
+        for (Object item : list){
+            newArray[index++] = item; //copy each element of arr into newArr
+        }
+        list = newArray;
     }
 
 }
